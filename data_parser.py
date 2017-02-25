0ae48eed9c3ece59ab3f7fe59c2713a1e28a7b67
Pradeep Pujari <ppujari@Pradeeps-MacBook-Pro.local>
2017-02-24 19:10:42 -0800

    code change after testing

---
+++ data_parser.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+
+import csv
+import cv2
+import matplotlib.image as mpimg
+import numpy as np
+from sklearn.utils import shuffle
+import traceback
+
+
+
+class DataParser:
+  """Prepare the data from image files and csv file"""
+
+  def __init__(self):
+    self._img_height = 160
+    self._img_width_original = 320
+    self._img_channels = 3
+    self._filename = 'data/driving_log.csv'
+
+  # grabs steering angles and filenames
+  def _grab_data(self):
+    self._file_IDs = []
+    steering_angles_list = []
+    with open(self._filename, 'r') as f:
+      try:
+        reader = csv.reader(f)
+        firstline = True
+        for row in reader:
+          if firstline:    #skip first line
+            firstline = False
+            continue
+          self._file_IDs.append(row[0].split("center_",1)[1])
+          steering_angles_list.append(float(row[3]))
+      finally:
+        f.close()
+    self._steering_angles = np.asarray(steering_angles_list)
+    self._file_IDs, self._steering_angles = shuffle(self._file_IDs, self._steering_angles, random_state=0)
+
+  # Returns YUV version of an image
+  #  xDiv_ and yDiv_ should be 2^#
+  def _grab_img_YUV(self, filename_, xDiv_, yDiv_):
+    img_BGR = cv2.imread(filename_)                  #gives BGR
+    img_BGR = cv2.resize(img_BGR, None, fx=1/xDiv_, fy=1/yDiv_, 
+                         interpolation = cv2.INTER_AREA)
+    #return cv2.cvtColor(img_BGR, cv2.COLOR_BGR2YUV)  #gives YUV
+    return img_BGR
+
+  def _grab_left_img(self, file_ID_, xDiv_, yDiv_):
+    return self._grab_img_YUV('data/IMG/left_' + file_ID_, xDiv_, yDiv_)
+
+  def _grab_center_img(self, file_ID_, xDiv_, yDiv_):
+    return self._grab_img_YUV('data/IMG/center_' + file_ID_, xDiv_, yDiv_)
+
+  def _grab_right_img(self, file_ID_, xDiv_, yDiv_):
+    return self._grab_img_YUV('data/IMG/right_' + file_ID_, xDiv_, yDiv_)
+
+  def _combine_batch(self, start_, stop_, xDiv_, yDiv_):
+    num_imgs = stop_-start_
+    self._left_imgs = np.zeros((num_imgs, int(self._img_height/yDiv_), int(self._img_width_original/xDiv_), 3))
+    self._center_imgs = np.zeros((num_imgs, int(self._img_height/yDiv_), int(self._img_width_original/xDiv_), 3))
+    self._right_imgs = np.zeros((num_imgs, int(self._img_height/yDiv_), int(self._img_width_original/xDiv_), 3))
+    index = 0
+    for img_num in range(start_, stop_):
+      self._left_imgs[index] = self._grab_left_img(self._file_IDs[img_num], xDiv_, yDiv_)
+      self._center_imgs[index] = self._grab_center_img(self._file_IDs[img_num], xDiv_, yDiv_)
+      self._right_imgs[index] = self._grab_right_img(self._file_IDs[img_num], xDiv_, yDiv_)
+      index += 1
+ 
+
+  '''
+  External API
+  '''
+  def grab_data_info(self):
+    self._grab_data()
+
+  def combine_batch(self, start, stop, xDiv, yDiv):
+    self._combine_batch(start, stop, xDiv, yDiv)
+
+  def parse_data(self):
+    self._grab_data()
+
+  @property
+  def img_height(self):
+    return self._img_height
+
+  @property
+  def img_width(self):
+    return self._img_width_original
+
+  @property
+  def img_channels(self):
+    return self._img_channels
+
+  @property
+  def steering_angles(self):
+    return self._steering_angles
+
+  @property
+  def left_imgs(self):
+    return self._left_imgs
+
+  @property
+  def center_imgs(self):
+    return self._center_imgs
+
+  @property
+  def right_imgs(self):
+    return self._right_imgs
+
+
+
+if __name__ == '__main__':
+  print('Running main in data_parser.py')
+
+  try:
+    data_parser = DataParser()
+    data_parser.parse_data()
+    data_parser.combine_batch(0, 3, 2, 2)
+    print(data_parser.center_imgs.shape)
+  except:
+    print(traceback.format_exc())
+
--- model.py
+++ model.py
@@ -1,82 +1,248 @@
-from keras.models import Sequential, save_model
-from keras.layers import Dense, Activation, Flatten, Dropout
+#!/usr/bin/env python
+
+import cv2
+import numpy as np
+import json
+from random import randint
+import traceback
+
+from keras.models import Sequential
+from keras.layers import Dense, Lambda
+from keras.layers import Flatten, Activation, Dropout
 from keras.layers.convolutional import Convolution2D
-from keras.layers.normalization import BatchNormalization
-from keras.callbacks import Callback
+from keras.layers import MaxPooling2D
+from keras.layers import ELU, LeakyReLU
+from keras.regularizers import l2
 
-import helper
-
-
-class SaveEpoch(Callback):
-    """Extend class of keras Callback to save model every epoch.
-    """
-    def on_epoch_end(self, epoch, logs={}):
-        save_model(self.model, '{}.h5'.format(epoch + 1))
+from data_parser import DataParser
 
 
-def get_model():
-    model = Sequential()
 
-    model.add(BatchNormalization(input_shape=(64, 64, 3)))
+class BehaviorCloner:
+  """Create Keras model to train learn from driving images in simulator and 
+     learn to control the car on it's own"""
 
-    model.add(Convolution2D(24, 5, 5, subsample=(2, 2)))
-    model.add(Activation('elu'))
+  def __init__(self):
+    self._data_parser = DataParser()
 
-    model.add(Convolution2D(36, 5, 5, subsample=(2, 2)))
-    model.add(Activation('elu'))
+  def _flip_image(self, img_):
+    return cv2.flip(img_, 1)
 
-    model.add(Convolution2D(48, 5, 5, subsample=(2, 2)))
-    model.add(Activation('elu'))
+  def _combine_LCR(self, labels_, epoch_):
+    left_imgs = self._data_parser.left_imgs
+    center_imgs = self._data_parser.center_imgs
+    right_imgs = self._data_parser.right_imgs
 
-    model.add(Convolution2D(64, 3, 3))
-    model.add(Activation('elu'))
+    angle_adjust = 0.1 
+    left_labels = np.copy(labels_) + angle_adjust
+    center_labels = np.copy(labels_)
+    right_labels = np.copy(labels_) - angle_adjust
 
-    model.add(Convolution2D(64, 3, 3))
-    model.add(Activation('elu'))
+    batch_size = left_imgs.shape[0]
+    row_size = left_imgs.shape[1]
+    col_size = left_imgs.shape[2]
+    total_imgs = np.zeros((batch_size, row_size, col_size, 3))
+    total_labels = np.zeros(batch_size)
+    for pic_num in range(total_imgs.shape[0]):
+      while 1:
+        # get index
+        index = randint(0,total_imgs.shape[0]-1)
+        # pick different images
+        lrc_rand = randint(0,100)
+        if lrc_rand > 66:
+          img   = right_imgs[index]
+          label = right_labels[index]
+        elif lrc_rand > 33:
+          img   = center_imgs[index]
+          label = center_labels[index]
+        else:
+          img   = left_imgs[index]
+          label = left_labels[index]
 
-    model.add(Flatten())
+        # probability says we shouldn't keep it
+        if (abs(label)*100 + epoch_*10) < randint(0,100):
+          continue
 
-    model.add(Dense(1164))
-    model.add(Activation('elu'))
+        # flip images
+        flip_rand = randint(0,100)
+        if flip_rand > 50:
+          img = self._flip_image(img)
+          label *= -1
 
-    model.add(Dense(100))
-    model.add(Activation('elu'))
+        # add and go to next in for loop
+        total_imgs[pic_num] = img
+        total_labels[pic_num] = label
+        break
 
-    model.add(Dense(50))
-    model.add(Activation('elu'))
+    return total_imgs, total_labels
 
-    model.add(Dense(10))
-    model.add(Activation('elu'))
-    model.add(Dropout(0.2))
 
-    model.add(Dense(1))
+  def _generator_training(self, labels_, batch_size_, xDiv_, yDiv_):
+      def _f():
+          epoch = 0
+          max_epoch = 5
+          start = 0
+          end = start + batch_size_
+          num_imgs = labels_.shape[0]
+  
+          while True:
+              self._data_parser.combine_batch(start, end, xDiv_, yDiv_) #setup data
+              X_batch, y_batch = self._combine_LCR(labels_[start:end], epoch)  #get data
+              start += batch_size_
+              end += batch_size_
+              if start >= num_imgs:
+                start = 0
+                end = batch_size_
+                epoch += 1
+                if epoch >= max_epoch:
+                  epoch = 0
+              if end >= num_imgs:
+                end = num_imgs
+  
+              yield (X_batch, y_batch)
+  
+      return _f
 
-    return model
+  def _generator_validation(self, labels_, batch_size_, xDiv_, yDiv_):
+      def _f():
+          start = 0
+          end = start + batch_size_
+          num_imgs = labels_.shape[0]
+  
+          while True:
+              self._data_parser.combine_batch(start, end, xDiv_, yDiv_) #setup data
+              X_batch = self._data_parser.center_imgs
+              y_batch = labels_[start:end]
+              start += batch_size_
+              end += batch_size_
+              if start >= num_imgs:
+                start = 0
+                end = batch_size_
+              if end >= num_imgs:
+                end = num_imgs
+  
+              yield (X_batch, y_batch)
+  
+      return _f
+
+  '''
+  External API
+  '''
+  def setup_data(self):
+    self._data_parser.parse_data()
+
+  # Build model based on
+  # Nvidia "End to End Learning for Self-Driving Cars"
+  def build_model(self, xDiv_, yDiv_):
+
+    input_height = int(self._data_parser.img_height/yDiv_)
+    input_width = int(self._data_parser.img_width/xDiv_)
+    input_channels = self._data_parser.img_channels
+
+    self._model = Sequential()
+
+    # normalize -1<>+1
+    self._model.add(Lambda(lambda x: x/127.5 - 1.,
+              input_shape=(input_height, input_width, input_channels),
+              output_shape=(input_height, input_width, input_channels)))
+
+
+    # Conv Layer #0 (depth=3, kernel=1x1) - change color space
+    self._model.add(Convolution2D(3, 1, 1, border_mode='same'))
+
+    # Conv Layer #1 (depth=24, kernel=5x5)
+    self._model.add(Convolution2D(24, 5, 5, border_mode='valid'))
+    self._model.add(ELU())
+    self._model.add(MaxPooling2D(pool_size=(2,2)))
+    self._model.add(Dropout(0.5))
+
+    # Conv Layer #2 (depth=36, kernel=5x5)
+    self._model.add(Convolution2D(36, 5, 5, border_mode='valid'))
+    self._model.add(ELU())
+    self._model.add(MaxPooling2D(pool_size=(2,2)))
+    self._model.add(Dropout(0.5))
+
+    # Conv Layer #3 (depth=48, kernel=3x3)
+    self._model.add(Convolution2D(48, 3, 3, border_mode='valid'))
+    self._model.add(ELU())
+    self._model.add(MaxPooling2D(pool_size=(2,2)))
+    self._model.add(Dropout(0.5))
+
+    # Conv Layer #4 (depth=64, kernel=3x3)
+    self._model.add(Convolution2D(64, 3, 3, border_mode='valid'))
+    self._model.add(ELU())
+    self._model.add(MaxPooling2D(pool_size=(2,2)))
+    self._model.add(Dropout(0.5))
+
+    self._model.add(Flatten())
+
+    # Hidden Layer #1
+    self._model.add(Dense(100))
+    self._model.add(ELU())
+
+    # Hidden Layer #2
+    self._model.add(Dense(50))
+    self._model.add(ELU())
+
+    # Hidden Layer #3
+    self._model.add(Dense(10))
+    self._model.add(ELU())
+
+    # Answer
+    self._model.add(Dense(1))
+    self._model.summary()
+
+
+
+  def train_model(self, num_epochs_, batch_size_, xDiv_, yDiv_):
+    print('BehaviorCloner: train_model()...')
+
+    # setup for training
+    self._model.compile(optimizer="adam", loss="mse")
+
+    # train the model
+    train_gen = self._generator_training(self._data_parser.steering_angles,
+                                         batch_size_, xDiv_, yDiv_)
+    num_imgs_train = self._data_parser.steering_angles.shape[0]*3   #3x for left, center, right
+    history = self._model.fit_generator(train_gen(), num_imgs_train, num_epochs_)
+
+    # validation
+    validation_gen = self._generator_validation(self._data_parser.steering_angles,
+                                                batch_size_, xDiv_, yDiv_)
+    num_imgs_validate = self._data_parser.steering_angles.shape[0]   #1x center
+    accuracy = self._model.evaluate_generator(validation_gen(), num_imgs_validate)
+    print("Accuracy = ", accuracy)
+
+    print('... train_model() done')
+
+
+  def save_model(self):
+    model_json = self._model.to_json()
+    with open('model.json', 'w') as outfile:
+      json.dump(model_json, outfile)
+
+    self._model.save_weights('model.h5')
 
 
 if __name__ == '__main__':
-    epochs = 20
-    batch_size = 128
+  print('Running main in model.py')
 
-    model = get_model()
-    model.compile('adam', 'mse')
+  try:
+    behavior_cloner = BehaviorCloner()
+    behavior_cloner.setup_data()
 
-    driving_data = helper.get_data_from_log()
-    train, val = helper.train_validation_split(driving_data)
+    x_down_sample = 5
+    y_down_sample = 2.5
+    behavior_cloner.build_model(x_down_sample, y_down_sample)
 
-    train_gen = helper.generate_batch(train, batch_size)
-    val_gen = helper.generate_batch(val, batch_size)
+    test_num_epochs = 5
+    test_batch_size = 16
+    behavior_cloner.train_model(test_num_epochs, test_batch_size, 
+                                x_down_sample, y_down_sample)
 
-    samples_per_epoch = len(train)
-    nb_val_samples = len(val)
+    behavior_cloner.save_model()
 
-    callbacks = [SaveEpoch()]
+    print('... main done')
+  except:
+    print(traceback.format_exc())
 
-    history = model.fit_generator(train_gen,
-                                  samples_per_epoch=samples_per_epoch,
-                                  nb_epoch=epochs,
-                                  callbacks=callbacks,
-                                  validation_data=val_gen,
-                                  nb_val_samples=nb_val_samples)
-
-    save_model(model, 'model.h5')
